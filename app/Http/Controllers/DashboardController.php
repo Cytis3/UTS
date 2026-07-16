595fdcc0d5d2ec42b54b158f41d11638462154de
Cytis3 <ikanterisedap@gmail.com>
2026-07-16 10:57:55 +0700

    .

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,23 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Pelanggan;
 use App\Models\Pesanan;
 use App\Models\Transaksi;
-use Illuminate\Http\Request;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
         // Menghitung total data untuk masing-masing entitas
-        $totalPelanggan = Pelanggan::count();
-        $totalPesanan   = Pesanan::count();
-        $totalTransaksi = Transaksi::count();
+        $totalPelanggan = Pelanggan::query()->count('*');
+        $totalPesanan = Pesanan::query()->count('*');
+        $totalTransaksi = Transaksi::query()->count('*');
 
         // Mengambil 5 data pesanan terbaru beserta data pelanggannya
         // (Sesuai aturan: Pesanan belongsTo Pelanggan)
         $pesananTerbaru = Pesanan::with('pelanggan')
-                                ->latest()
-                                ->take(5)
-                                ->get();
+            ->latest()
+            ->take(5)
+            ->get();
 
         return view('dashboard', compact(
             'totalPelanggan',
